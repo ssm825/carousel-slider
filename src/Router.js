0262018b5c2c7f7e4f3f543389ddb3ade5733279
ssm825 <ssm39825@gmail.com>
2022-12-01 03:24:45 +0900

    Init : 초기 설정

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import Main from 'pages/Main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
