27be80b56c3e48b75368b420f3118a31e0de0c34
ssm825 <ssm39825@gmail.com>
2023-04-24 14:19:29 +0900

    Rename : slider content 상수 데이터로 분리

---
+++ src/constants/sliderData.js
@@ -0,0 +1,26 @@
+export const SLIDER_DATA = [
+  {
+    id: 1,
+    color: '#38e5ff',
+  },
+  {
+    id: 2,
+    color: '#ffcb6b',
+  },
+  {
+    id: 3,
+    color: '#929292',
+  },
+  {
+    id: 4,
+    color: '#38e5ff',
+  },
+  {
+    id: 5,
+    color: '#ffcb6b',
+  },
+  {
+    id: 6,
+    color: '#929292',
+  },
+];
